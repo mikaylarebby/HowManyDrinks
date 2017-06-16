3913e015e4da59fb61a3aae7fa282dbce6b77919
MikaylaT <mikaylat@mikaylat.sramarketing.com>
2017-06-16 14:32:04 -0500

    rough draft of how many drinks tool

---
+++ HowManyDrinks.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.lang.*;
+import java.util.*;
+
+public class HowManyDrinks {
+
+	public static void main(String args[]) {
+		
+
+
+	}
+	
+	public static String calculateDrinks(int age, int weight, String gender, String drinkType, int drinkNum, int hours) {
+		
+		double ouncesConsumed = 0;
+		double BAC = 0;
+		
+		
+		if (drinkType == "Wine") {
+			ouncesConsumed = (drinkNum * 5)*(0.12);
+		}
+		if (drinkType == "Beer") {
+			ouncesConsumed = (drinkNum * 12)*(0.05);
+		}
+		if (drinkType == "The Hard Stuff") {
+			ouncesConsumed = (drinkNum * 1.5)*(0.40);
+		}
+		if (gender == "Male") {
+			BAC = ((ouncesConsumed * 5.14)/(weight * 0.73)) - (0.015 * hours);
+		}
+		if (gender == "Female") {
+			BAC =  ((ouncesConsumed * 5.14)/(weight * 0.66)) - (0.015 * hours);
+		}
+		
+		String output = new String();
+		output = String.format("%.2f", BAC);
+		
+		if(BAC < 0.08) {
+			
+			return "Yes you can drive. You are not drunk, your BAC is " + output;
+		}
+		else {
+			return "No, you cannot drive. You are drunk, your BAC is " + output;
+		}
+		
+	}
+
+}
